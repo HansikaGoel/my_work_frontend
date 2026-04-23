1fb46d16f84a5672dab3c12639137890883c7d8e
Hansika Goel <hansikagoel3008@gmail.com>
2026-04-23 20:22:46 +0530

    frontend

---
+++ javaScriptPrograms/button.js
@@ -0,0 +1,6 @@
+    alert(75-20);
+    alert(" Welcome Danger");
+    console.log("Hello Friends!");
+    console.log("Bharat Mata Ki Jai");
+    console.warn("This is a warning message, don't dare to mess with me!");
+    console.error("the above message is a blunder");
